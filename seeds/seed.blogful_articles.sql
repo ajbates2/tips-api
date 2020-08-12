523c89e074fa3adfd7f4678b034cbb878dcb616d
AJBates2 <aj.bates2.92@gmail.com>
2020-08-12 01:52:01 -0500

    init commit

---
+++ seeds/seed.blogful_articles.sql
@@ -0,0 +1,5 @@
+INSERT INTO blogful_articles (title, style, content)
+VALUES
+  ('First post!', 'Interview',
+    'Example Seed'),
+  ('Second', 'Example', 'Seed')
---
+++ src/app.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const validateBearerToken = require('./token-validator')
+
+const app = express()
+
+const morganOption = (NODE_ENV === 'production')
+    ? 'tiny'
+    : 'dev';
+
+app.use(morgan(morganOption))
+app.use(helmet())
+app.use(cors())
+
+//app.use(validateBearerToken) turn back on when ready for authorized requests
+
+app.get('/', (req, res) => {
+    res.send('Hello, Boilerplate!')
+})
+
+app.use(function errorHandler(error, req, res, next) {
+    let response
+    if (NODE_ENV === 'production') {
+        response = { error: { message: 'server error' } }
+    }
+    else {
+        console.error(error)
+        response = { message: error.message, error }
+    }
+    res.status(500).json(response)
+})
+
+module.exports = app
