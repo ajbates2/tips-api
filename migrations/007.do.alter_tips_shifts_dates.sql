03286ead9536d295aac3c10a392b8e04758fbef7
AJ Bates <aj.bates2.92@gmail.com>
2021-02-25 00:53:43 -0600

    added new date columns

---
+++ migrations/007.do.alter_tips_shifts_dates.sql
@@ -0,0 +1,8 @@
+ALTER TABLE tips_shifts
+    ADD COLUMN work_day TEXT;
+
+ALTER TABLE tips_shifts
+    ADD COLUMN work_week TEXT;
+
+ALTER TABLE tips_shifts
+    ADD COLUMN work_month TEXT;
---
+++ migrations/007.undo.alter_tips_shifts_dates.sql
@@ -0,0 +1,8 @@
+ALTER TABLE tips_roles
+    DROP COLUMN IF EXISTS work_day;
+
+ALTER TABLE tips_roles
+    DROP COLUMN IF EXISTS work_month;
+
+ALTER TABLE tips_roles
+    DROP COLUMN IF EXISTS work_week;
