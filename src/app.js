4d081674622b689e551813285afffcf150e53105
AJ Bates <aj.bates2.92@gmail.com>
2021-03-04 23:19:20 -0600

    forced cors

--- src/app.js
+++ src/app.js
@@ -1,51 +1,54 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const shiftsRouter = require('./shifts/shifts-router')
-const usersRouter = require('./users/users-router')
-const authRouter = require('./auth/auth-router')
-const paychecksRouter = require('./paychecks/paychecks-router')
-const jobsRouter = require('./jobs/jobs-router')
-const rolesRouter = require('./roles/roles-router')
-const shiftPatchRouter = require('./shifts/shift-patch-router')
-const checkPatchRouter = require('./paychecks/check-patch-router')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const shiftsRouter = require('./shifts/shifts-router');
+const usersRouter = require('./users/users-router');
+const authRouter = require('./auth/auth-router');
+const paychecksRouter = require('./paychecks/paychecks-router');
+const jobsRouter = require('./jobs/jobs-router');
+const rolesRouter = require('./roles/roles-router');
+const shiftPatchRouter = require('./shifts/shift-patch-router');
+const checkPatchRouter = require('./paychecks/check-patch-router');
 
-const app = express()
+const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-    ? 'tiny'
-    : 'dev'
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'dev';
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+	next();
+});
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Hello, Boilerplate!')
-})
+	res.send('Hello, Boilerplate!');
+});
 
-app.use('/api/shifts', shiftsRouter)
-app.use('/api/paychecks', paychecksRouter)
-app.use('/api/users', usersRouter)
-app.use('/api/auth', authRouter)
-app.use('/api/jobs', jobsRouter)
-app.use('/api/roles', rolesRouter)
-app.use('/api/shifts/patch', shiftPatchRouter)
-app.use('/api/paychecks/patch', checkPatchRouter)
+app.use('/api/shifts', shiftsRouter);
+app.use('/api/paychecks', paychecksRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/jobs', jobsRouter);
+app.use('/api/roles', rolesRouter);
+app.use('/api/shifts/patch', shiftPatchRouter);
+app.use('/api/paychecks/patch', checkPatchRouter);
 
 app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    }
-    else {
-        console.error(error)
-        response = { message: error.message, error }
-    }
-    res.status(500).json(response)
-})
+	let response;
+	if (NODE_ENV === 'production') {
+		response = { error: { message: 'server error' } };
+	} else {
+		console.error(error);
+		response = { message: error.message, error };
+	}
+	res.status(500).json(response);
+});
 
-module.exports = app
+module.exports = app;
