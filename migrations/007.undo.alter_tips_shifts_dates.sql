c49d86ef2879a8ff08f765190b6705e32735a058
AJ Bates <aj.bates2.92@gmail.com>
2021-03-04 20:48:41 -0600

    heroku update

--- migrations/007.undo.alter_tips_shifts_dates.sql
+++ migrations/007.undo.alter_tips_shifts_dates.sql
@@ -1,11 +1,11 @@
-ALTER TABLE tips_roles
+ALTER TABLE tips_shifts
     DROP COLUMN IF EXISTS work_day;
 
-ALTER TABLE tips_roles
+ALTER TABLE tips_shifts
     DROP COLUMN IF EXISTS work_month;
 
-ALTER TABLE tips_roles
+ALTER TABLE tips_shifts
     DROP COLUMN IF EXISTS work_week;
 
-ALTER TABLE tips_roles
+ALTER TABLE tips_shifts
     DROP COLUMN IF EXISTS work_year;
