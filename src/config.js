e8ee83aeef2b37ae5f18f5ec21ea0f401cee7861
AJ Bates <aj.bates2.92@gmail.com>
2021-03-04 21:26:25 -0600

    updated cors

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://master:1@localhost/tips',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://aj:pass@localhost/tips',
     JWT_SECRET: process.env.JWT_SECRET || 'make-that-shmoney',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
