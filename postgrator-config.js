88ab97cee0acd850dcf5084cea48ecdcaabc10b7
AJ Bates <aj.bates2.92@gmail.com>
2021-03-04 21:16:17 -0600

    almost have postgrator working

--- postgrator-config.js
+++ postgrator-config.js
@@ -7,5 +7,7 @@ module.exports = {
         ? process.env.TEST_DATABASE_URL
         : process.env.DATABASE_URL,
     "ssl": !!process.env.SSL,
-    "password": "pass"
+    "password": (process.env.NODE_ENV === 'development')
+        ? 'pass'
+        : process.env.PASSWORD
 }
