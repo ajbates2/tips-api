2e588e86a6154f32a8712050c7212e760a63ba54
AJ Bates <aj.bates2.92@gmail.com>
2021-03-19 11:28:22 -0500

    update cors options

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(morgan(morganOption));
 app.use(helmet());
 
 app.use(
-	cors({ origin: 'https://www.tipsdontlie.com/', optionsSuccessStatus: 200 })
+	cors({ origin: /tipsdontlie\.com$/, optionsSuccessStatus: 200 })
 );
 
 app.get('/', (req, res) => {
