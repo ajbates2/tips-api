a0bd55f0ad2240459d837b5b0274e5c723247520
AJ Bates <aj.bates2.92@gmail.com>
2021-03-22 18:06:19 -0500

    testing with azure

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'dev';
 app.use(morgan(morganOption));
 app.use(helmet());
 
-app.use(cors({ origin: /tipsdontlie\.com$/ }));
+app.use(cors());
 
 app.get('/', (req, res) => {
 	res.send('Hello, Boilerplate!');
